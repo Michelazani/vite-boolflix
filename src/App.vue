360994f8e0f0380ff1fce92ea0d21962521d5943
Michelazani <michi.zani220@gmail.com>
2023-12-20 12:40:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import appHeader from './components/appHeader.vue';
+import appMain from './components/appMain.vue'
+
+
+export default {
+  components:{
+    appHeader,
+    appMain
+  },
+}
+</script>
+
+<template>
+  <appHeader/>
+  <appMain/>
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+
+</style>
---
+++ src/components/appMain.vue
@@ -0,0 +1,18 @@
+<script >
+export default{
+    name: 'appMain',
+    data() {
+        return {
+        
+        }
+    }
+}
+</script>
+
+<template>
+    
+</template> 
+
+<style lang="scss" scoped>
+
+</style>

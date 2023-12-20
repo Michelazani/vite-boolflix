aa39e1e907c47c1a94e4623360ad7364e5802eff
Michelazani <michi.zani220@gmail.com>
2023-12-20 21:33:56 +0100

    v-model e funzione per craere nuovo url unito alla richiesta dell'utente

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -6,8 +6,11 @@ export default {
     data() {
         return {
             store,
+            searchMovies: '',
             movies: [],
-            moviesApiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=af2d007dc49307ea5757760954dfd889&query=vampire'
+            moviesApiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=af2d007dc49307ea5757760954dfd889&query=vampire',
+            // faccio in modo che si aggiunga alla parola ricercata dall'utente
+            apiUrlNoQuery: 'https://api.themoviedb.org/3/search/movie?api_key=af2d007dc49307ea5757760954dfd889&query=',
         }
     },
     name: 'appMain',
@@ -25,6 +28,19 @@ export default {
                     // handle error
                     console.log(error);
                 })
+        },
+        filterMovies(){
+            axios.get(this.apiUrlNoQuery)
+                .then((response) => {
+                    // handle success
+                    this.store.moviesList = this.apiUrlNoQuery + this.searchMovies;
+                    console.log(this.store.moviesList);
+
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
         }
     },
     created(){
@@ -35,8 +51,8 @@ export default {
 
 <template>
     <div class="input-group m-5">
-      <input type="text" class="" placeholder="Type a movie" aria-label="Type a movie" aria-describedby="button-addon2">
-      <button class="btn btn-dark" type="button" id="button-addon2">Search</button>
+      <input type="text" class="" placeholder="Type a movie" aria-label="Type a movie" aria-describedby="button-addon2" v-model="searchMovies">
+      <button class="btn btn-dark" type="button" id="button-addon2" @click="filterMovies()">Search</button>
     </div>
 
 
